df91f280ae399429003a0df5453ce1b6e923bd61
MilkyMish <Mik1997626@rambler.ru>
2024-01-08 18:27:47 +0300

    main config declared

---
+++ git-lfs-synchronizer/Heplers/JsonUtils.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace git_lfs_synchronizer.Heplers
+{
+    public static class JsonUtils
+    {
+        public static bool CheckJsonFile(string path)
+        {
+            if (!File.Exists(path))
+            {
+                Console.WriteLine($"File {path} is not found.");
+                return false;
+            }
+
+            var data = File.ReadAllText(path);
+
+            if (string.IsNullOrWhiteSpace(data))
+            {
+                Console.WriteLine($"File {path} is empty.");
+                return false;
+            }
+
+            try
+            {
+                var jToken = JObject.Parse(data);
+                if (jToken is JObject)
+                {
+                    return true;
+                }
+
+                Console.WriteLine($"Contents of file {path} shall be a json object");
+                return false;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"File to parse file {path} as json: {e.Message}");
+                return false;
+            }
+        }
+    }
+}
--- git-lfs-synchronizer/Program.cs
+++ git-lfs-synchronizer/Program.cs
@@ -1,8 +1,22 @@
 using System.Text.Json.Serialization;
 using System.Text.Json;
 using git_lfs_synchronizer.Services;
+using git_lfs_synchronizer.Configuration;
+using git_lfs_synchronizer.Heplers;
+
+var configurationFile = $"etc{Path.DirectorySeparatorChar}config.json";
+
+if (!JsonUtils.CheckJsonFile(configurationFile))
+{
+    throw new Exception($"Incorrect configuration file: {configurationFile}");
+}
 
 var builder = WebApplication.CreateBuilder(args);
+
+var config = new MainConfiguration();
+builder.Configuration.AddJsonFile(configurationFile, true, true);
+builder.Configuration.Bind(config);
+builder.Services.AddSingleton(config);
 
 builder.Services.AddControllers().AddJsonOptions(o =>
 {
@@ -20,6 +34,19 @@ var serializerOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web)
 
 builder.Services.AddSingleton<LfsService>();
 builder.Services.AddHostedService<ClientService>();
+
+
+/*builder.Services.AddHttpClient("GitLfsSynchronizerClient", client =>
+{
+    client.BaseAddress = uriBuilder.Uri;
+});*/
+
+/*builder.Services.AddHttpClient("GitLfsSynchronizerClient", client =>
+{
+    client.BaseAddress = uriBuilder.Uri;
+});*/
+
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
